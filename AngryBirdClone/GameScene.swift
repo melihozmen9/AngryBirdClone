b567ca1b490e4ce1ba218c2582fd21199e4e7aa7
Melih Özmen <melihozmen9@gmail.com>
2022-11-21 11:57:33 +0300

    21-Nov
    Kutular tanımlanmaya başlandı.

--- AngryBirdClone/GameScene.swift
+++ AngryBirdClone/GameScene.swift
@@ -11,17 +11,36 @@ import GameplayKit
 class GameScene: SKScene {
     
 var dragon = SKSpriteNode()
+    var box1 = SKSpriteNode()
+    var box2 = SKSpriteNode()
+    var box3 = SKSpriteNode()
+    var box4 = SKSpriteNode()
+    var box5 = SKSpriteNode()
+    var box6 = SKSpriteNode()
+    
     
     
     override func didMove(to view: SKView) {
+        // PhysicsBody
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        self.scene?.scaleMode = .aspectFit
+        
+        // Dragon
         
         let dragonTexture = SKTexture(imageNamed: "dragon")
         dragon = childNode(withName: "dragon") as! SKSpriteNode
-        dragon.physicsBody = SKPhysicsBody(circleOfRadius: dragonTexture.size().height/5)
+        dragon.physicsBody = SKPhysicsBody(circleOfRadius: dragonTexture.size().height/17)
         dragon.physicsBody?.isDynamic = true
         dragon.physicsBody?.affectedByGravity = true
         dragon.physicsBody?.mass = 0.5
         
+        // boxes
+        let brickTexture = SKTexture(imageNamed: "brick")
+        
+        box1 = childNode(withName: "box1") as! SKSpriteNode
+        box1.physicsBody?.affectedByGravity = true
+        box1.physicsBody?.isDynamic = true
+        box1.physicsBody?.mass = 0.4
         
        
     }
